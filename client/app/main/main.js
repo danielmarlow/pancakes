b4ba892d8bf876d4f52aa50e39865645f8eff485
Daniel Marlow <dan@danielmarlow.com>
2014-07-30 15:32:31 -0600

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pancakesApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
